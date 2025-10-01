305ce66716b0c6057841966d47ac637d27cfef64
Yelyzaveta Baranovska <lizabaranovska133@gmail.com>
2025-10-01 20:05:20 +0300

    timer and snaskbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const delay = parseInt(formData.get('delay'));
+    const state = formData.get('state');
+
+    createPromise(delay, state)
+      .then(delay => {
+        console.log(`✅ Fulfilled promise in ${delay}ms`);
+        iziToast.show({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+          timeout: 5000,
+          backgroundColor: '#4CAF50',
+          theme: 'dark',
+          progressBar: false,
+          close: false,
+          displayMode: 2,
+        });
+      })
+      .catch(delay => {
+        console.log(`❌ Rejected promise in ${delay}ms`);
+        iziToast.show({
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+          timeout: 5000,
+          backgroundColor: '#F44336',
+          theme: 'dark',
+          progressBar: false,
+          close: false,
+          displayMode: 2,
+        });
+      });
+
+    form.reset();
+  });
+
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  }
+});
